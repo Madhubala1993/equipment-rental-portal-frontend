3deac1ca4b0c14dc3c173839bd36a4eb464fb701
Madhubala1993 <madhu.a1993@gmail.com>
2022-04-16 11:07:48 +0530

    final

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
 // export const API = "http://localhost:9000";
-export const API = "https://equipmentp-rental-portal.herokuapp.com/";
+export const API = "https://equipmentp-rental-portal.herokuapp.com";
